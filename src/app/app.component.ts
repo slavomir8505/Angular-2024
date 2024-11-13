ae4ee59397441b0d9ce47889e425a83b24d5999f
slavomir8505 <slavomirkana@gmail.com>
2024-11-13 10:09:35 +0100

    17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { LibraryComponent } from "./components/library/library/library.component
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LibraryComponent,MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, LibraryComponent,MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { LibraryComponent } from './components/library/library/library.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'shoppingcart-component', component: ShoppingcartComponent},
+    {path: 'library-component', component: LibraryComponent},
+];
