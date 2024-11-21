a96501bfd26eafa7475a95cacbddf0c5c7ceeb7a
slavomir8505 <slavomirkana@gmail.com>
2024-11-21 09:12:15 +0100

    19

--- src/app/components/library/library/library.component.ts
+++ src/app/components/library/library/library.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Book } from '../../../models/book';
 import { BookComponent } from '../book/book.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { LibraryService } from '../../../library.service';
 
 @Component({
   selector: 'app-library',
@@ -15,40 +16,10 @@ import {MatButtonModule} from '@angular/material/button';
 })
 
 export class LibraryComponent {
+  libraryservice: LibraryService=inject(LibraryService)
 
-  books: Book[] = [
-    {
-      id: 1,
-      title: 'Harry Potter 1 a Kameň mudrcov',
-      author: 'Joanne K. Rowling',
-      genre: 'Fantasy',
-      available: true,
-      pages: '256 pages'
-    },
-    {
-      id: 2,
-      title: 'Harry Potter 2 - A tajomná komnata',
-      author: 'Joanne K. Rowling',
-      genre: 'Fantasy',
-      available: false,
-      pages: '360 pages'
-    },
-    {
-      id: 3,
-      title: 'Harry Potter 3 - A väzeň z Azkabanu',
-      author: 'Joanne K. Rowling',
-      genre: 'Fantasy',
-      available: true,
-      pages: '440 pages'
-    },
-    {
-      id: 4,
-      title: 'Harry Potter 4 - A ohnivá čaša',
-      author: 'Joanne K. Rowling',
-      genre: 'Fantasy',
-      available: true,
-      pages: '712 pages'
-    },
-
-  ];
+  books: Book[]
+  constructor() {
+    this.books=this.libraryservice.getAllBooks()
+  }
 }
---
+++ src/app/library.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Book } from './models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibraryService {
+  books: Book[] = [
+    {
+      id: 1,
+      title: 'Harry Potter 1 a Kameň mudrcov',
+      author: 'Joanne K. Rowling',
+      genre: 'Fantasy',
+      available: true,
+      pages: '256 pages'
+    },
+    {
+      id: 2,
+      title: 'Harry Potter 2 - A tajomná komnata',
+      author: 'Joanne K. Rowling',
+      genre: 'Fantasy',
+      available: false,
+      pages: '360 pages'
+    },
+    {
+      id: 3,
+      title: 'Harry Potter 3 - A väzeň z Azkabanu',
+      author: 'Joanne K. Rowling',
+      genre: 'Fantasy',
+      available: true,
+      pages: '440 pages'
+    },
+    {
+      id: 4,
+      title: 'Harry Potter 4 - A ohnivá čaša',
+      author: 'Joanne K. Rowling',
+      genre: 'Fantasy',
+      available: true,
+      pages: '712 pages'
+    },
+
+  ];
+  constructor() { }
+  getAllBooks(): Book[]{
+    return this.books;
+  }
+}
