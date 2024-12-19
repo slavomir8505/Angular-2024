05342288b0548009de486c6cb929c8c5c7d69ea2
filipriec <filippriec@gmail.com>
2024-12-19 09:12:30 +0100

    fixed code

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,37 +1,45 @@
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatFormFieldModule}
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule,MatFormFieldModule],
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.css'
 })
-
-
 export class AddBookComponent {
-bookForm: FormGroup;
+  bookForm: FormGroup;
 
-constructor(private fb: FormBuilder){
-  this.bookForm= this.fb.group({
-    title: ['', Validators.required],
-    author: ['', Validators.required],
-    year: ['', [Validators.required, Validators.min(0)]],
-    copies: ['', [Validators.required, Validators.min(1)]],
-    genre: ['', Validators.required],
-  })
-}
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      year: ['', [Validators.required, Validators.min(0)]],
+      copies: ['', [Validators.required, Validators.min(1)]],
+      genre: ['', Validators.required],
+    });
+  }
 
-onsubmit() {
-  if (this.bookForm.valid){
-    console.log('New Book:', this.bookForm.value);
+  onSubmit() {  // Changed from onsubmit to onSubmit to match template
+    if (this.bookForm.valid) {
+      console.log('New Book:', this.bookForm.value);
+    }
+  }
+
+  onClear() {
+    this.bookForm.reset();
   }
 }
-
-onClear() {
-  this.bookForm.reset();
-}
-}
-import { Component } from '@angular/core';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 import { LibraryComponent } from './components/library/library/library.component';
-import {AddBook} from 
+import { AddBookComponent } from './add-book/add-book.component';  // Changed from AddBook to AddBookComponent
 
 export const routes: Routes = [
     {path: 'shoppingcart-component', component: ShoppingcartComponent},
     {path: 'library-component', component: LibraryComponent},
-    {path: }
-    
+    {path: 'add-book', component: AddBookComponent}  // Changed from AddBook to AddBookComponent
 ];
