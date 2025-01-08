c30fe436986863a450937a5203ea65f1c8ab63f7
slavomir8505 <slavomirkana@gmail.com>
2025-01-08 09:40:30 +0100

    77

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1BPhvKTu6Ai6GiT080GlQHwIemlRBd5U",
+  authDomain: "angular-a94fc.firebaseapp.com",
+  projectId: "angular-a94fc",
+  storageBucket: "angular-a94fc.firebasestorage.app",
+  messagingSenderId: "305128178236",
+  appId: "1:305128178236:web:da16677c4e9f3c11b03939",
+  measurementId: "G-0X0QHTC33V"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(), provideAnimationsAsync()]
+  providers: [importProvidersFrom(
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(), provideAnimationsAsync()]
 };
+
--- src/app/library.service.ts
+++ src/app/library.service.ts
@@ -3,19 +3,61 @@ import { Book } from './models/book';
 import { of, Observable, delay,pipe,throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { catchError,tap } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryService {
+  booklist: Book[] = [ 
+    
+      {
+        id: 1,
+        title: 'Harry Potter 1 a Kameň mudrcov',
+        author: 'Joanne K. Rowling',
+        genre: 'Fantasy',
+        available: true,
+        pages: '256 pages'
+      },
+      {
+        id: 2,
+        title: 'Harry Potter 2 - A tajomná komnata',
+        author: 'Joanne K. Rowling',
+        genre: 'Fantasy',
+        available: false,
+        pages: '360 pages'
+      },
+      {
+        id: 3,
+        title: 'Harry Potter 3 - A väzeň z Azkabanu',
+        author: 'Joanne K. Rowling',
+        genre: 'Fantasy',
+        available: true,
+        pages: '440 pages'
+      },
+      {
+        id: 4,
+        title: 'Harry Potter 4 - A ohnivá čaša',
+        author: 'Joanne K. Rowling',
+        genre: 'Fantasy',
+        available: true,
+        pages: '712 pages'
+      }
+    
+  ]
 
-  link: string = "https://run.mocky.io/v3/08a2307a-f2b3-4f13-8e3e-8237f5f9e2dc"
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient, private store: AngularFirestore) {
+    /*this.booklist.forEach(entry =>{
+      this.addData(entry);
+    });*/
+  }
+  addData(data:Book){
+    this.store.collection('books').add(data);
+  }
   getAllBooks(): Observable<Book[]> {
-    return this.http
+  /*  return this.http
     .get<Book[]>(this.link)
-    .pipe(delay(2000),tap((data) => console.log('GET data:', data)));
+    .pipe(delay(2000),tap((data) => console.log('GET data:', data)));*/
+    return of()
   }
 }
